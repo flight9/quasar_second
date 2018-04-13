30538f6fd0e30041ea672e56b969d7d046bb884c
flight9 <ealeaf@sohu.com>
2018-04-13 09:18:13 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <!-- Don't drop "q-app" class -->
+  <div id="q-app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+/*
+ * Root component
+ */
+export default {
+  mounted () {
+    // zm: test current route
+    // (this will not get correct value)
+    console.info('App this router:', this.$router)
+    console.info('App current route:', this.$route)
+
+    // zm: construct a one-time watcher by LinusBorg
+    // (this will get correct value)
+    this.routeWatcher = this.$watch(
+      function () { return this.$route },
+      function (route) {
+        if (route.name === 'Launch') {
+          console.log('App watched route', this.$route)
+          // this.authenticate() // or whatever
+          this.routeWatcher() // this disables the watcher
+        }
+      }
+    )
+  }
+}
+</script>
+
+<style></style>
---
+++ src/components/Launch.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2>Launch</h2>
+    <h5 class="subtitle">(test this router)</h5>
+    <h4>code:(nothing)</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style>
+  .subtitle {
+    color: lightgrey;
+    margin-top: -30px;
+  }
+</style>
